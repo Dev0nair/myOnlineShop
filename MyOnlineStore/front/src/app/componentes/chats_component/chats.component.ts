1cd9dcf859a575c994651305c132c14323bac26f
dev0nair <3268.ismael@gmail.com>
2019-08-15 21:10:07 +0200

    carpeta del proyecto insertada

---
+++ MyOnlineStore/front/src/app/componentes/chats_component/chats.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chats',
+  templateUrl: './chats.component.html',
+  styleUrls: ['./chats.component.css']
+})
+export class ChatsComponent {
+  title = 'chats';
+}
---
+++ MyOnlineStore/front/src/app/componentes/props_component/props.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'props',
+  templateUrl: './props.component.html',
+  styleUrls: ['./props.component.css']
+})
+export class PropsComponent {
+  title = 'props';
+}
